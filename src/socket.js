59cbd2a15f66d5bc36914548c1b03d4ed96b2594
grintoma <matt.grinton@gmail.com>
2021-11-27 19:39:13 -0500

    Added socket.io to allow for current bid functionallity

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import io from 'socket.io-client'
 
-export const Socket = io('http://localhost:54545')
+export const Socket = io('https://mattgrinton.com/api')
 
 export const SocketContext = React.createContext()
